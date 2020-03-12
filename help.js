b7b0cae7167c7e83665b568143ff4327645d50a6
markdavepanado <markdavepanado@gmail.com>
2020-03-12 17:51:41 +0800

    Header Portfolio Links and Help Container Added

---
+++ help.js
@@ -0,0 +1,15 @@
+const helpButton = document.getElementById("helpImg");
+
+helpButton.addEventListener("click", help);
+
+function help() {
+  const helpTextContainer = document.getElementsByClassName(
+    "help-text-container"
+  );
+  //   console.log("clicked");
+  //   console.log(this);
+
+  //   console.log(helpTextContainer[0]);
+
+  helpTextContainer[0].classList.toggle("active");
+}
